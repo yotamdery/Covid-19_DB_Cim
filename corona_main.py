6bdbb31df7a40b22ff02db39d4a8ab5d6fbbe2c7
“yotamdery”git config --global user.name “yotamdery <yotamdery@gmail.com>
2021-01-13 17:58:40 +0200

    first commit

---
+++ corona_main.py
@@ -0,0 +1,337 @@
+#!/usr/bin/python
+# coding: utf-8
+#Imports:
+import pandas as pd
+import numpy as np
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+import mysql.connector as mdb,sys
+from sqlalchemy import create_engine
+import pymysql
+import matplotlib.pyplot as plt
+import os
+
+pymysql.install_as_MySQLdb()
+from health_data import sick_with_pop_socio_grouped as health_file
+
+# Main Class of our application
+class InputForm(BoxLayout):
+    'User Input form'
+    'All references from KV file should come here'
+    # Variables from the kivy file.
+    ip = ObjectProperty()
+    results = ObjectProperty()
+
+    # Creating the connection with MySQL DB, Creating a new Data-Base with a query
+    def connect(self,):
+        'Building new DB and new table if not exists already'
+        con = mdb.connect(host = self.ip.text, user = 'root', passwd = '12yotam3')
+        cur = con.cursor()
+        cur.execute('CREATE DATABASE if not exists corona_db')
+        con.commit()  # Makes transaction
+        con.close()  # Close transaction
+
+    # Creating the method which will allow the user to insert his input to the MySQL DB:
+    def insert(self):
+        'Inserting user input to DB'
+    #    df = pd.read_csv(r'sick_with_pop_socio_grouped.csv')
+        df = health_file
+        host = self.ip.text
+        user = 'root'
+        passwd = '12yotam3'
+        engine = create_engine("mysql://" + user + ':' + passwd + '@' + host + '/corona_db')
+        con = engine.connect()
+        df.to_sql(con=con, name='corona_data', if_exists='replace', schema= 'corona_db', index= True, chunksize= 100)
+        con.close()
+
+
+    def display_data_town_level(self):
+        'Create new screen, initialize data_town_level'
+        # Clear all widgets from the InputForm.
+        self.clear_widgets()
+        # Add a new form - data town level. Initialize the data town level form with the ip we already have.
+        self.add_widget(show_data_town_level(self.ip.text))
+
+    def display_data_national_level(self):
+        'Create new screen, initialize data_national_level'
+        # Clear all widgets from the InputForm.
+        self.clear_widgets()
+        # Add a new form - data national level. Initialize the data national level form with the ip we already have.
+        self.add_widget(show_data_national_level(self.ip.text))
+
+    def display_sample_from_db(self):
+        'Create new screen, initialize ShowRecords'
+        # Clear all widgets from the InputForm.
+        self.clear_widgets()
+        # Add a new form - sample from db. Initialize the sample from db form with the ip we already have.
+        self.add_widget(show_sample_from_db(self.ip.text))
+
+
+    def show(self, rows, features= None):
+        'Show all query results on screen'
+        # Clear previous results.
+        self.results.text=''
+        if not rows:
+            # If we didn't receive any rows to show, display 'No records'.
+            self.results.text='No records'
+        else:
+           #If there are any rows to show, Iterate over the rows to show them.
+            if features != None:         # If there's a need to show the columns, It means that there's only one row to show.
+                for column in features:
+                    self.results.text += str(column) + "    "
+                self.results.text += '\n'
+
+                spacing = ""  # Defining dynamic spacing between values of the row
+                for row in rows:
+                    for i in range(len(row)):   # indexing
+                        self.results.text += str(row[i]) + spacing
+                        if i <= 3:
+                            spacing = "                                 "
+
+            else:                       # If there's no need to show the columns
+                for row in rows:
+                    for element in row:
+                        self.results.text += str(element) + "  "
+                    self.results.text += '\n'
+
+    def out(self):
+        'User close the application'
+        sys.exit()
+
+
+class show_sample_from_db(BoxLayout):
+    'Display query results to user'
+    # Variables from the kivy file.
+    results = ObjectProperty()
+
+    # Inherit the show method from InputForm class
+    show_sample = InputForm.show
+
+    out_sample = InputForm.out
+
+    def __init__(self, ip):
+        'Make a select * query pass all result rows to method show'
+        super(show_sample_from_db, self).__init__()
+        # Save the ip we got to this instance.
+        self.ip = ip
+        # Connect to the db.
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("select date, town_code, town from corona_data order by rand() Limit 5;")
+        rows = cur.fetchall()
+        # Call show with the rows we got.
+        self.show_sample(rows)
+
+    def go_home(self):
+        # Create new screen
+        self.clear_widgets()
+        # Send back to InputForm
+        self.add_widget(InputForm())
+
+
+class show_data_town_level(BoxLayout):
+    'Display query results to user'
+    # Variables from the kivy file.
+    results = ObjectProperty()
+    town = ObjectProperty()
+
+    def __init__(self, ip):
+        'Make a select * query pass all result rows to method show'
+        super(show_data_town_level, self).__init__()
+        # Save the ip we got to this instance.
+        self.ip = ip
+
+    def show_total_pop(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT DISTINCT(population) FROM corona_data WHERE town like '%{}%';".format(self.town.text))
+        rows = cur.fetchall()
+        # Call show with the rows we got.
+        self.show_method_town_level(rows)
+
+    def show_socioeconomic_score(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT DISTINCT(Poverty_De) FROM corona_data WHERE town like '%{}%';".format(self.town.text))
+        rows = cur.fetchall()
+        rows.append(['\n','The socio-economic score is scaled between 1 to 10 as follows:', '\n', 'The score 1 represents the highest class, and scor 10 represents the poorest'])
+        # Call show with the rows we got.
+        self.show_method_town_level(rows)
+
+    def show_health_data(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        features = ['town        ','accumulated_tested','accumulated_cases','accumulated_recoveries','active_cases','positivity_rate']
+        cur.execute("SELECT town,accumulated_tested,accumulated_cases,accumulated_recoveries,active_cases,positivity_rate FROM corona_data where town like '%{}%' ORDER BY date DESC LIMIT 1;".format(self.town.text))
+        rows = cur.fetchall()
+        # Call show with the rows we got.
+        self.show_method_town_level(rows, features)
+
+    def show_trend_figure_town(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT date, active_cases, population  FROM corona_data WHERE town like '%{}%';".format(self.town.text))
+        rows = cur.fetchall()
+
+        # Creating a Dataframe from the plot
+        df = pd.DataFrame(rows)
+        df.columns = ['date', 'active_cases', 'population']
+        df.set_index('date', inplace= True)
+        df['active_cases_per_pop'] = (df['active_cases'] / df['population'].iloc[0] ) * 100
+        df.drop(columns= ['active_cases', 'population'], axis = 1, inplace= True)
+
+        # Call show with the rows we got.
+        self.show_figure(df, self.town.text)
+
+    def show_figure(self, df, town):
+        # Creating the figure
+        figure = df.plot().get_figure()
+        plt.ylabel("Percantage of active cases")
+        plt.title("Percantage of active cases - over time")
+        plt.xticks(rotation=20)
+        plt.rcParams["figure.figsize"] = (10, 5)
+        if os.path.exists('figure_trend_over_time_town.jpeg'):
+            os.remove(r'figure_trend_over_time_town.jpeg')
+        figure.savefig('figure_trend_over_time_town.jpeg')
+        # Create new screen to show the figure
+        self.clear_widgets()
+        # Add a new form - sample from db. Initialize the sample from db form with the ip we already have.
+        self.add_widget(screen_for_trend_town())
+
+
+    # Inherit the show method from InputForm class
+    show_method_town_level = InputForm.show
+
+    # Inherit the out method from InputForm class
+    out_method_town_level = InputForm.out
+
+    # Inherit the go home method from show_sample_from_db class
+    go_home_town_level = show_sample_from_db.go_home
+
+# A class to show the figure of the trend over time on the town level
+class screen_for_trend_town(BoxLayout):
+    # Inherit the out method from InputForm class
+    out_method_screen_for_trend = InputForm.out
+    # Inherit the go home method from show_sample_from_db class
+    go_home_screen_for_trend = show_sample_from_db.go_home
+
+
+class show_data_national_level(BoxLayout):
+    'Display query results to user'
+    # Variables from the kivy file.
+    results = ObjectProperty()
+
+    def __init__(self, ip):
+        'Make a select * query pass all result rows to method show'
+        super(show_data_national_level, self).__init__()
+        # Save the ip we got to this instance.
+        self.ip = ip
+
+    def show_color_prevalence(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT town_color, COUNT(DISTINCT(town)) FROM corona_data GROUP BY town_color;")
+        rows = cur.fetchall()
+        # Call show with the rows we got.
+        self.show_method_national_level(rows)
+
+    def show_town_highest_positive_cases(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT town, accumulated_cases FROM corona_data ORDER BY date DESC, accumulated_cases DESC LIMIT 1;")
+        rows = cur.fetchall()
+        # Call show with the rows we got.
+        self.show_method_national_level(rows)
+
+    def show_national_active_cases(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT date, SUM(active_cases) as active_cases FROM corona_data GROUP BY date ORDER BY date;")
+        rows = cur.fetchall()
+
+        # Creating a Dataframe from the plot
+        df = pd.DataFrame(rows)
+        df.columns = ['date', 'active cases']
+        df.set_index('date', inplace=True)
+        df['active_cases'] = df['active cases'].astype(float)
+        # Call show with the rows we got.
+        self.show_active_cases_figure(df)
+
+    def show_active_cases_figure(self, df):
+        # Creating the figure
+        figure = df.plot().get_figure()
+        plt.ylabel("Active cases")
+        plt.title("National active cases - over time")
+        plt.xticks(rotation=20)
+        plt.rcParams["figure.figsize"] = (10, 5)
+        figure.savefig('figure_active_cases_national.jpeg')
+        # Create new screen to show the figure
+        self.clear_widgets()
+        # Add a new form - sample from db. Initialize the sample from db form with the ip we already have.
+        self.add_widget(screen_for_national_active_cases())
+
+    def show_national_positivity_rate(self):
+        con = mdb.connect(host=self.ip, user='root', passwd='12yotam3', database='corona_db')
+        cur = con.cursor()
+        # Query the db to get all stored rows.
+        cur.execute("SELECT date, ROUND(SUM(count_positive_cases) * 100 / SUM(count_tests),2) as positivity_rate FROM corona_data GROUP BY date ORDER BY date;")
+        rows = cur.fetchall()
+
+        # Creating a Dataframe from the plot
+        df = pd.DataFrame(rows)
+        df.columns = ['date', 'positivity rate']
+        df.set_index('date', inplace=True)
+        # Call show with the rows we got.
+        self.show_positivity_rate_figure(df)
+
+    def show_positivity_rate_figure(self,df):
+        figure = df.plot().get_figure()
+        plt.ylabel("Positivity rate")
+        plt.title("National positivity rate - over time")
+        plt.xticks(rotation=20)
+        plt.rcParams["figure.figsize"] = (10, 5)
+        figure.savefig('figure_positivity_rate_national.jpeg')
+        # Create new screen to show the figure
+        self.clear_widgets()
+        # Add a new form - sample from db. Initialize the sample from db form with the ip we already have.
+        self.add_widget(screen_for_national_positivity_rate())
+
+
+    # Inherit the show method from InputForm class
+    show_method_national_level = InputForm.show
+
+    # Inherit the show method from InputForm class
+    out_method_national_level = InputForm.out
+
+    # Inherit the go home method from show_sample_from_db class
+    go_home_national_level = show_sample_from_db.go_home
+
+# A class to show the figure of national active cases
+class screen_for_national_active_cases(BoxLayout):
+    # Inherit the show method from InputForm class
+    out_method_screen_for_active_cases = InputForm.out
+    # Inherit the go home method from show_sample_from_db class
+    go_home_screen_for_national_active_cases = show_sample_from_db.go_home
+
+# A class to show the figure of national positivity rate
+class screen_for_national_positivity_rate(BoxLayout):
+    # Inherit the show method from InputForm class
+    out_method_screen_for_active_cases = InputForm.out
+    # Inherit the go home method from show_sample_from_db class
+    go_home_screen_for_national_positivity_rate = show_sample_from_db.go_home
+
+
+class Corona_kivyApp(App):
+    pass
+
+if __name__ == '__main__':
+    Corona_kivyApp().run()
---
+++ health_data.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+# Importing:
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import urllib.request
+import plotly.express as px
+import warnings 
+warnings.simplefilter('ignore')
+import datetime
+desired_width=320
+pd.set_option('display.max_columns',10)
+pd.set_option('display.width', desired_width)
+
+# Display format
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+# Accessing the API
+url = 'https://data.gov.il/api/3/action/datastore_search?resource_id=d07c0771-01a8-43b2-96cc-c6154e7fa9bd&limit=1000000'
+fileobj = urllib.request.urlopen(url)
+df = pd.read_json(fileobj)
+data = df.loc['records']['result']
+sick = pd.DataFrame(data)
+
+# In[2]:
+
+#creating a mapper of town_code to town
+town_code_to_town = sick[['town_code', 'town']]
+town_code_to_town['town_code'] = town_code_to_town['town_code'].astype(int)
+
+# In[3]:
+
+# Type convertion of fields
+sick['date'] = pd.to_datetime(sick['date'])
+sick = sick.rename(columns= {'accumulated_diagnostic_tests' : 'accumulated_tested'})
+sick = sick.drop('_id', axis = 1)
+
+# filtering the df - Taking only numerical amounts and excluding nulls in the # of tests column
+sick_filtered = sick[(sick['accumulated_cases'] != '0') & (sick['accumulated_recoveries'] != '0') & (sick['accumulated_tested'] != '0')]
+sick_filtered = sick_filtered[(sick_filtered['accumulated_cases'] != '<15') & (sick_filtered['accumulated_recoveries'] != '<15') & (sick_filtered['accumulated_tested'] != '<15')]
+sick_filtered = sick_filtered.loc[sick_filtered['accumulated_tested'].isna() == False]
+
+sick_filtered['accumulated_tested'] = sick_filtered['accumulated_tested'].astype(float)
+
+# In[4]:
+
+# Converting to int for all numeric fields
+for c in ['town_code','accumulated_cases','accumulated_recoveries','accumulated_tested']:
+    sick_filtered[c] = sick_filtered[c].astype(int)
+
+# In[5]:
+
+# Grouping the file by date and townn, And creating the 'active cases' column
+sick = sick_filtered.groupby(['date', 'town_code']).sum().reset_index()
+sick['active_cases'] = sick['accumulated_cases'] - sick['accumulated_recoveries']
+sick = sick.sort_values(by = ['town_code', 'date'])
+
+# In[6]:
+# Filtering towns that don't have more than a week documented (after cleaning)
+def no_data_for_a_week(df):
+    if df.shape[0] < 7:
+        pass
+    else:
+        return df
+sick = sick.groupby('town_code', as_index= False).apply(no_data_for_a_week).reset_index()
+sick = sick.drop(labels= ['level_0','level_1'], axis= 1)
+
+sick = sick.merge(town_code_to_town, how= 'left', left_on= 'town_code', right_on= 'town_code')
+
+# In[7]:
+
+sick = sick.drop_duplicates(subset= ['date', 'town_code'])
+
+# In[8]:
+
+# Converting from accumulated values to Count values, Creating 2 new columns:
+def create_count_heealth(df):
+    df["count_tests"] = df['accumulated_tested'].diff(periods = 1)
+    df['count_positive_cases'] = df['accumulated_cases'].diff(periods = 1)
+    df['count_active_cases'] = df['active_cases'].diff(periods = 1)
+    df = df.loc[df["count_tests"].notna()]
+    return df
+
+# In[9]:
+
+sick_with_count_health = sick.groupby('town_code', as_index= False).apply(create_count_heealth).reset_index()
+sick_with_count_health = sick_with_count_health.reset_index().drop(labels= ['index','level_0', 'level_1'], axis = 1)
+
+# In[10]:
+
+sick_with_count_health['positivity_rate'] = (sick_with_count_health['count_positive_cases'] / sick_with_count_health['count_tests']) * 100
+
+
+# ### importing the population file:
+
+# In[11]:
+
+# Creating a mapper with population for each town
+pop_file = pd.read_excel(r'סהכ-אוכלוסייה-לפי-אס-גיל-ומין-סוף-2017.xlsx', engine= 'openpyxl', usecols= 'B,D,E')
+pop_file = pop_file.loc[pop_file['אזור סטטיסטי'] == 'סה"כ']
+pop_file = pop_file.rename(columns= {'אוכלוסייה בסוף 2017' : 'population', 'סמל יישוב' : 'town_id' }).drop('אזור סטטיסטי', axis = 1)
+pop_file['population'] = pop_file['population'].astype(int)
+pop_file['town_id'] = pop_file['town_id'].astype(int)
+
+
+# In[12]:
+
+
+sick_with_pop = sick_with_count_health.merge(pop_file, how= 'left', left_on= 'town_code', right_on= 'town_id').drop('town_id', axis = 1)
+
+# ### Envolving the socio demographic data
+
+# In[13]:
+
+socio_df = pd.read_csv(r'sociodemographic_stat.csv')
+socio_df_grouped = socio_df.groupby(by= 'SEMEL_YISH')['Poverty_De'].mean()
+
+# In[14]:
+
+sick_pop_poverty = sick_with_pop.merge(socio_df_grouped, how= 'left', left_on= 'town_code', right_on= 'SEMEL_YISH')
+
+# In[15]:
+
+# A mapper to fill null values
+town_socio_mapper = sick_pop_poverty[['town_code','Poverty_De']].loc[~sick_pop_poverty[['town_code','town']].duplicated()]
+sick_pop_poverty.drop('Poverty_De', axis = 1, inplace = True)
+
+
+# In[16]:
+
+# Handeling null values
+np.random.seed(100)
+def fill_na_values(row): 
+    row['Poverty_De'] = row['Poverty_De'].fillna(np.random.uniform(low= 0, high= 10))
+    return row
+
+# In[17]:
+
+town_socio_mapper = town_socio_mapper.groupby(by= 'town_code').apply(fill_na_values)
+
+# In[18]:
+
+sick_with_pop_socio = sick_with_pop.merge(town_socio_mapper, how= 'left', left_on= 'town_code', right_on= 'town_code')
+
+# ### Creating the indexes for traffic light model
+
+# #### index number 1:
+# In[19]:
+
+sick_with_pop_socio['sick_for_10,000'] = sick_with_pop_socio['population'] * sick_with_pop_socio['count_active_cases'] / 10000
+
+# In[20]:
+
+sick_with_pop_socio['sick_for_10,000'][sick_with_pop_socio['sick_for_10,000'] < 1] = 1
+
+# #### index number 2:
+
+# In[21]:
+
+def create_index_2(df):
+    last_week_active_cases = list(df.loc[df['date'] == (df['date'].max() - datetime.timedelta(days=6))]['active_cases'])[0]
+    todays_positive_cases = list(df.loc[df['date'] == df['date'].max()]['count_positive_cases'])[0]
+    try:    
+        df['growth_rate'] = ((todays_positive_cases / last_week_active_cases)) ** 2
+        return df
+    except ZeroDivisionError:
+        df['growth_rate'] = 0.01
+        return df
+
+# In[22]:
+
+sick_with_pop_socio = sick_with_pop_socio.groupby(by= 'town_code').apply(create_index_2)
+
+# In[23]:
+
+sick_with_pop_socio['growth_rate'][sick_with_pop_socio['growth_rate'] < 0.5] = 1
+
+# #### index number 3:
+
+# In[24]:
+
+# taking the rows within the past week:
+mask = (sick_with_pop_socio['date'] > (sick_with_pop_socio['date'].max() - datetime.timedelta(days=7))) & (sick_with_pop_socio['date'] <= sick_with_pop_socio['date'].max())
+df_past_week = sick_with_pop_socio.loc[mask]
+
+
+# In[25]:
+
+df_past_week_grouped = df_past_week.groupby('town_code')['positivity_rate'].mean().to_frame().reset_index()
+
+# In[26]:
+
+df_past_week_grouped['positivity_rate_last_week'] = (((df_past_week_grouped['positivity_rate'])+0.25 / 100)) * (100/8)
+
+# In[27]:
+
+df_past_week_grouped = df_past_week_grouped.drop('positivity_rate', axis = 1)
+
+# In[28]:
+
+# Merging it with the health df
+sick_with_pop_socio = sick_with_pop_socio.merge(df_past_week_grouped, how= 'left', left_on= 'town_code', right_on= 'town_code')
+
+# In[29]:
+
+sick_with_pop_socio.head()
+
+# ### Creating the traffic-light index for each town
+
+# In[30]:
+
+# The traffic light formula as it defined by Israel's ministrey of health
+sick_with_pop_socio['traffic_light_index'] = 2 + np.log(sick_with_pop_socio['sick_for_10,000']) + np.log(sick_with_pop_socio['growth_rate']) + np.log(sick_with_pop_socio['positivity_rate_last_week'])
+
+# In[31]:
+
+# Determine the color of the town according to the traffic light model
+def set_town_color(df):
+    if list(df['traffic_light_index'])[0] >= 7.5:
+        df['town_color'] = 'red'
+    elif (list(df['traffic_light_index'])[0] < 7.5) and (list(df['traffic_light_index'])[0] >= 6):
+        df['town_color'] = 'orange'
+    elif (list(df['traffic_light_index'])[0] < 6) and (list(df['traffic_light_index'])[0] >= 4.5):
+        df['town_color'] = 'yellow'
+    else:
+        df['town_color'] = 'green'
+    return df
+
+# In[32]:
+
+sick_with_pop_socio_grouped = sick_with_pop_socio.groupby('town_code').apply(set_town_color)
+
+# In[33]:
+### Importing the towns hebrew english file to have the town names in english
+towns_hebrew_english = pd.read_csv(r'towns_hebrew_english.csv', encoding= 'utf-8')
+sick_with_pop_socio_grouped = sick_with_pop_socio_grouped.merge(towns_hebrew_english, how= 'inner', left_on= 'town_code', right_on= 'town_code')
+sick_with_pop_socio_grouped['town'] = sick_with_pop_socio_grouped['town_name']
+sick_with_pop_socio_grouped.drop('town_name', axis= 1, inplace= True)
+
+sick_with_pop_socio_grouped = round(sick_with_pop_socio_grouped, 2)
+
+# In[ ]:
+sick_with_pop_socio_grouped['town'] = sick_with_pop_socio_grouped['town'].str.replace("'", "")
+sick_with_pop_socio_grouped.to_csv(r"sick_with_pop_socio_grouped.csv", index = False)
+
+
